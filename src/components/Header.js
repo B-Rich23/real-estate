c92a868a81e1266c486dc197deafb0a258f4f79d
Brandan Richardson <brandanr@gmail.com>
2018-07-23 00:56:21 -0700

    added some styling to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,17 @@ import React, { Component } from 'react';
 class Header extends Component {
     state = {  }
     render() {
-        return (<div>Header</div>   );
+        return (
+            <header>
+                <div className="logo">Logo</div>
+                <nav>
+                    <a href="#">Create Ads</a>
+                    <a href="#">About Us</a>
+                    <a href="#">Log In</a>
+                    <a href="#"className="register-btn">Register</a>
+
+                </nav>
+            </header>   );
     }
 }
 
