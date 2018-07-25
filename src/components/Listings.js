4f9b8c2306d54926b22312c13e32253eb161dbf4
Brandan Richardson <brandanr@gmail.com>
2018-07-25 11:03:44 -0700

    added styling to the serach area of the Listings component

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -8,16 +8,16 @@ class Listings extends Component {
         return (
             <section id="listings">
                 <section className="search-area">
-                    <input type="text" name="search" />
+                    <input type="text" name="search" placeholder="Search" />
                 </section>
                 <section className="sortby-area">
-                    <div>240 results found</div>
+                    <div className="sort-results">240 results found</div>
                     <div className="sort-options">
                         <select name="sortby" className="sortby">
                             <option value="price-asc">Highest Price</option>
                             <option value="price-dsc">Lowest Price</option>
                         </select>
-                        <div className="view">
+                        <div className="sort-view">
                         <i className="fas fa-list"></i>
                         <i className="fas fa-th"></i>
                         </div>
