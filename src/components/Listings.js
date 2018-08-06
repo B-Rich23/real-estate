d809d66acde3aa1927d8c4fec1430e4e06eadac9
Brandan Richardson <brandanr@gmail.com>
2018-08-06 14:57:02 -0700

    started adding react modal component to Listings component

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+// import ReactModal from './ReactModal';
+import  Modal  from 'react-responsive-modal';
+
 
 
 
 class Listings extends Component {
 
     render() {
-        const { listings, open } = this.props;
+        const { listings, open, modal } = this.props;
+        const lorem = (
+            <p>
+              Mauris ac arcu sit amet dui interdum bibendum a sed diam. Praesent
+              rhoncus congue ipsum elementum lobortis. Ut ligula purus, ultrices id
+              condimentum quis, tincidunt quis purus. Proin quis enim metus. Nunc
+              feugiat odio at eros porta, ut rhoncus lorem tristique. Nunc et ipsum eu
+              ex vulputate consectetur vel eu nisi. Donec ultricies rutrum lectus, sit
+              ame feugiat est semper vitae. Proin varius imperdiet consequat. Proin eu
+              metus nisi. In hac habitasse platea dictumst. Vestibulum ac ultrices
+              risus. Pellentesque arcu sapien, aliquet sed orci sit amet, pulvinar
+              interdum velit. Nunc a rhoncus ipsum, maximus fermentum dolor. Praesent
+              aliquet justo vitae rutrum volutpat. Ut quis pulvinar est.
+            </p>
+          );
 
         return (
             <section id="listings">
@@ -61,6 +78,15 @@ class Listings extends Component {
                         </div>
 
                         })}
+                        <Modal open={modal} onClose={this.onCloseModal}>
+          <h2>Big modal</h2>
+          {lorem}
+          {lorem}
+          {lorem}
+          {lorem}
+          {lorem}
+          {lorem}
+        </Modal>
                     </section>
                 </div>
                 <section id="pagination">
@@ -76,6 +102,7 @@ class Listings extends Component {
                     </section>
 
             </section>
+
         );
     }
 }
